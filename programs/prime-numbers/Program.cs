0cee36e471cef237914d8d77944cbac42cbac2a1
Daniel Pilich <dpilich16@gmail.com>
2023-05-05 19:38:39 +0200

    First version of the program.

--- programs/prime-numbers/Program.cs
+++ programs/prime-numbers/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int number = 0;
+
+Console.Write("Input a number to check if it is a prime number: ");
+try
+{
+    number = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Wrong input. Not a number!!!");
+}
+
+bool result = true;
+for (int i = 2; i < number / 2; i++)
+{
+    if (number % i == 0)
+    {
+        result = false;
+        break;
+    }
+}
+
+if (result)
+    Console.WriteLine("This is a prime number :)");
+else
+    Console.WriteLine("This is not a prime number :(");

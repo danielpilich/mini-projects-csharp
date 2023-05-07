dea2148471ee88d0150ad4afa9221d78e1ab0013
Daniel Pilich <dpilich16@gmail.com>
2023-05-07 03:23:01 +0200

    Getting started with the program Factorial. Added user input.

---
+++ Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿int factorial = 0;
+Console.WriteLine("Input a number that will be your factorial: ");
+try
+{
+    factorial = Convert.ToInt32(Console.ReadLine());
+    if (factorial < 0) Console.WriteLine("Number need to be positive.");
+}
+catch
+{
+    Console.WriteLine("Wrong input. Not a number!!!");
+}
+

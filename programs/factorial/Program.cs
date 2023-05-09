6b9cf6debe7f750623f716e0e69317882e21736d
Daniel Pilich <dpilich16@gmail.com>
2023-05-09 23:14:46 +0200

    Add factorial calculation and modify user input

--- programs/factorial/Program.cs
+++ programs/factorial/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0;
+try
+{
+    Console.WriteLine("Input a number how many factorials do you want: ");
+    count = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Wrong input. Not a number!!!");
+}
+
+int[] factorial = new int[count];
+Console.WriteLine("Input a number/s that will be your factorial/s: ");
+for (int i = 0; i < count;)
+{
+    try
+    {
+        factorial[i] = Convert.ToInt32(Console.ReadLine());
+        if (factorial[i] < 0)
+        {
+            Console.WriteLine("Number have to be positive or zero. Try again: ");
+        }
+        else
+        {
+            i++;
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Wrong input. Not a number!!!");
+    }
+}
+
+for (int i = 0; i < factorial.Length; i++)
+{
+    Console.Write($"The value of factorial {factorial[i]} is: ");
+    if (factorial[i] > 1)
+    {
+        for (int j = factorial[i] - 1; j > 1; j--)
+        {
+            Console.WriteLine(factorial[i] + " " + j);
+            factorial[i] *= j;
+
+        }
+    }
+    else
+    {
+        factorial[i] = 1;
+    }
+    Console.WriteLine(factorial[i]);
+}
+
+

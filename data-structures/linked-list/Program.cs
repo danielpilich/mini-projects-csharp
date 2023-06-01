b7d4ebb425a9e676284755e04be4a1397b33bbbf
unknown <dpilich16@gmail.com>
2023-06-01 18:35:06 +0200

    Add method that inserts element to the list

--- data-structures/linked-list/Program.cs
+++ data-structures/linked-list/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Press any key to exit the program.");
+﻿LinkedList list = new LinkedList();
+list.Insertion(20);
+list.Insertion(64);
+list.Insertion(2);
+list.Insertion(53);
+
+Console.WriteLine("Press any key to exit the program.");
 Console.ReadKey();
 
 class LinkedList
@@ -10,6 +16,23 @@ class LinkedList
         public Element(int value)
         {
             this.value = value;
+        }
+    }
+    private Element first = null;
+    public void Insertion(int value)
+    {
+        if (first == null)
+        {
+            first = new Element(value);
+        }
+        else
+        {
+            Element tmp = first;
+            while (tmp.next != null)
+            {
+                tmp = tmp.next;
+            }
+            tmp.next = new Element(value);
         }
     }
 }

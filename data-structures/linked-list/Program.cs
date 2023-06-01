06d3e6ecc8024c9b222cd5605cfc5fec06f7224c
unknown <dpilich16@gmail.com>
2023-06-01 18:30:14 +0200

    Create class Element handling one element of the list

--- data-structures/linked-list/Program.cs
+++ data-structures/linked-list/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Press any key to exit the program.");
+Console.ReadKey();
+
+class LinkedList
+{
+    class Element
+    {
+        public int value;
+        public Element next;
+        public Element(int value)
+        {
+            this.value = value;
+        }
+    }
+}
